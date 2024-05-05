d1adbfaed90c156c8105f801a636555a75c975de
vall <vall020404@yandex.ru>
2024-05-05 10:15:07 +0300

    git init

---
+++ src/test/java/com/mycompany/pizzeriaapp/PizzeriaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.pizzeriaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
